f0054066864b0739630476d6de733d72e857ea3d
HaoyiShan <110078010+HaoyiShan@users.noreply.github.com>
2022-07-27 19:26:02 +0800

    Update broken_score.py

--- Prac 03/broken_score.py
+++ Prac 03/broken_score.py
@@ -1,10 +1,21 @@
-try:
-    score = float(input("Enter score: "))
-    if 90 > score >= 50:
-        print("Passable")
-    if score >= 90:
-        print("Excellent")
-    if score < 50:
-        print("Bad")
-except ValueError:
-    print("Invalid score")
+import random
+
+def main():
+    try:
+        score = random_score()
+        if 90 > score >= 50:
+            return "Passable"
+        if score >= 90:
+            return "Excellent"
+        if score < 50:
+            return "Bad"
+    except ValueError:
+        return "Invalid score"
+
+
+def random_score():
+    score = random.randint(0, 100)
+    return score
+
+
+print(main())
