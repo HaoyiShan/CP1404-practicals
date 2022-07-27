d6ee01f2113560e3488d11c2ab57b47c420085c1
HaoyiShan <110078010+HaoyiShan@users.noreply.github.com>
2022-07-27 19:25:23 +0800

    Update temperatures.py

--- Prac 03/temperatures.py
+++ Prac 03/temperatures.py
@@ -3,14 +3,26 @@ F - Convert Fahrenheit to Celsius
 Q - Quit"""
 print(MENU)
 choice = input(">>> ").upper()
+
+
+def CtF():
+    global fahrenheit
+    fahrenheit = celsius * 9.0 / 5 + 32
+
+
+def FtC():
+    global celsius
+    celsius = 5 / 9 * (fahrenheit - 32)
+
+
 while choice != "Q":
     if choice == "C":
         celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
+        CtF()
         print("Result: {:.2f} F".format(fahrenheit))
     elif choice == "F":
         fahrenheit = float(input("fahrenheit: "))
-        celsius = 5 / 9 * (fahrenheit - 32)
+        FtC()
         print("Result: {:.2f} C".format(celsius))
     else:
         print("Invalid option")
